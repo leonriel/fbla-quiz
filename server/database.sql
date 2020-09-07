86387802708c3a405bc3fde08edc996449c42d23
Gabriel <gabrielong2004@gmail.com>
2020-09-07 00:22:35 -0700

    Built routes with postgres queries

--- server/database.sql
+++ server/database.sql
@@ -24,18 +24,11 @@ CREATE TABLE questions(
     id SERIAL PRIMARY KEY,
     question VARCHAR(255) NOT NULL,
     answer VARCHAR(255) NOT NULL,
-    inserted_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
-
-CREATE TRIGGER update_timestamp 
-BEFORE UPDATE 
-ON questions FOR EACH ROW 
-EXECUTE PROCEDURE update_timestamp();
 
 CREATE TABLE results(
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    user_id INT NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
     score INT NOT NULL,
     inserted_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -46,4 +39,4 @@ BEFORE UPDATE
 ON results FOR EACH ROW 
 EXECUTE PROCEDURE update_timestamp();
 
-/*should I add a table for quiz types???*/
+/*should I add a table for quiz types??? Yes*/
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db")
+const format = require("pg-format")
 
 //middleware
 app.use(cors());
@@ -10,12 +11,66 @@ app.use(express.json());
 //ROUTES//
 
 //create a user
+app.post("/users", async (req, res) => {
+    try {
+        const { name } = req.body;
+        const newUser = await pool.query("INSERT INTO users (name) VALUES ($1)", [name]);
 
-//get a question
+        res.json(newUser.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+//get user
+app.get("/users/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+
+        res.json(user.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 
 //get questions
+app.get("/questions", async (req, res) => {
+    try {
+        const { ids } = req.body;
+        let query = format("SELECT * FROM questions WHERE id IN %L", [ids]);
+        const questions = await pool.query(query); 
+
+        res.json(questions.rows);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+//create/insert a result
+app.post("/results/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const { score } = req.body;
+        const newResult = await pool.query("INSERT INTO results (user_id, score) VALUES ($1, $2)", [userId, score])
+
+        res.json(newResult.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 
 //get result
+app.get("/results/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const result = await pool.query("SELECT * FROM results WHERE user_id = $1", [userId]);
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
