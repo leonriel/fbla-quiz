f0c7a39025265fb12a9d1d66a2348b3f663a1050
Leonard Gabriel Ong <45550118+leonriel@users.noreply.github.com>
2020-09-20 01:56:24 -0700

    Added quiz_types table (#8)

---
+++ server/migrations/sqls/20200920085044-create-quiz-types-table-down.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+DROP TABLE quiz_types;
---
+++ server/migrations/sqls/20200920085044-create-quiz-types-table-up.sql
@@ -0,0 +1,6 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE quiz_types(
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(255) NOT NULL
+)
