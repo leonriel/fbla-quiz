ce95ae6b0b3a768fded621cf8ef5c95bf67f17ab
Gabriel <gabrielong2004@gmail.com>
2020-09-20 02:16:48 -0700

    Added question types

---
+++ server/migrations/sqls/20200920091222-add-question-types-down.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+DELETE FROM question_types WHERE question_types.name IN ('multiple_choice', 'free_response', 'true_false');
---
+++ server/migrations/sqls/20200920091222-add-question-types-up.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+INSERT INTO question_types (name) VALUES ('multiple_choice'), ('free_response'), ('true_false');
