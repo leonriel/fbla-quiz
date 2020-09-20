4af3a564ec27ee04288abc438e174d2601983e15
Leonard Gabriel Ong <45550118+leonriel@users.noreply.github.com>
2020-09-20 02:05:39 -0700

    Added quiz types (#9)

---
+++ server/migrations/sqls/20200920085719-add-quiz-types-down.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+DELETE FROM quiz_types WHERE quiz_types.name = 'mixed' AND quiz_types.name = 'match';
---
+++ server/migrations/sqls/20200920085719-add-quiz-types-up.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+INSERT INTO quiz_types (name) VALUES ('mixed'), ('match');
